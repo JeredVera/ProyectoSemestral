7e7135857d96ef9bda963231f3ee019f9e5684d0
JeredVera <je.verap@duocuc.cl>
2023-08-25 11:18:03 -0400

    Version 1.1 Cargando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'cargando',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'conductores',
+    loadChildren: () => import('./pages/conductores/conductores.module').then( m => m.ConductoresPageModule)
+  },
+  {
+    path: 'pasajeros',
+    loadChildren: () => import('./pages/pasajeros/pasajeros.module').then( m => m.PasajerosPageModule)
+  },
+  {
+    path: 'cargando',
+    loadChildren: () => import('./pages/cargando/cargando.module').then( m => m.CargandoPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 
