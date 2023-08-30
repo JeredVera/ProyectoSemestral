770a65e19fddfc8718e570a1786a3b9f42853e0d
JeredVera <je.verap@duocuc.cl>
2023-08-30 13:13:35 -0400

    Version 3.0 Avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Cuenta', url: '/folder/inbox', icon: 'person' },
+    { title: 'Privacidad y Seguridad', url: '/folder/outbox', icon: 'lock-closed' },
+    { title: 'Mensajes', url: '/folder/favorites', icon: 'card' },
+    { title: 'Conductores', url: '/folder/archived', icon: 'car' },
+    { title: 'Terminos y condiciones', url: '/folder/trash', icon: 'warning' },
+    { title: 'Cerrar sesion', url: '/folder/spam', icon: 'log-out' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = [''];
   constructor() {}
 }
