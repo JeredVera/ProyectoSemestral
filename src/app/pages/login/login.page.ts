819e191d649104166ac3322643b1c811d55cf550
JeredVera <je.verap@duocuc.cl>
2023-10-08 22:47:13 -0300

    Version 12.0 Usuarios Random

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { UsuariosrandomService } from 'src/app/services/usuariosrandom.service';
 
 @Component({
   selector: 'app-login',
@@ -10,29 +12,33 @@ import { NavController } from '@ionic/angular';
 
 export class LoginPage implements OnInit { 
 
-  email: string = '';
-  password: string = '';
-  emailError: string = '';
-  passwordError: string = '';
+  //DECLARACION
+  loginForm: FormGroup //VALIDA EL FORMULARIO
+  user: any           //CAPTURA INFO USUARIO RANDOM
+  emailValue?: string //CAPTURA EMAIL USUARIO RANDOM
+  passValue?: string //CAPTURA PASS USUARIO RANDOM
 
-  constructor(private router: Router, private navCtrl: NavController) {}
+  constructor(private router: Router, private navCtrl: NavController, private usuariosrandom: UsuariosrandomService, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email:['', [Validators.required, Validators.email]],
+      password:['', [Validators.required, Validators.minLength(6)]],
+    })
+  }
 
 
   ngOnInit() {
+    this.usuariosrandom.getRandomUser().subscribe(
+      (data) =>{
+        //console.log(data)
+        this.user = data.results[0] //RELLENAMOS EL USUARIO
+        this.emailValue = this.user.email
+        this.passValue = this.user.login.password
+      })
   }
 
 
   login() {
-    this.emailError = 'Ingresa El Nombre de Usuario Correctamente';
-    this.passwordError = 'Ingresa La Contraseña Correctamente';
-
-    if (this.email === 'Cliente' && this.password === 'Cliente123') {
-      this.navCtrl.navigateForward('/home');
-    } else if (this.email === 'Conductor' && this.password === 'Conductor123'){
-      this.navCtrl.navigateForward('/home-conductores')
-    } else {
-      console.log('Nombre de Usuario y/o Contraseña Incorrecta');
-    }
+    this.navCtrl.navigateForward('/home');
   }
 
   register() {
